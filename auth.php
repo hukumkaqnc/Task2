92cb6e2d9ced76196999bb0dba13b7d07b6d87d8
nikitemailua <nikitemailua@gmail.com>
2023-04-13 08:29:47 +0300

    Correcting_mistakes_1

--- auth.php
+++ auth.php
@@ -2,5 +2,7 @@
 $title = "В-П семинар";
 $page_title = "Вход";
 $content = file_get_contents("content/form_auth.php");
+session_start();
+//$_SESSION['login'] =  $content['login']
 include("content/layout.php");
 ?>
