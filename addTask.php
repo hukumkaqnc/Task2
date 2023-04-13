fd855783476b625a87eb7fa44f6d0ef819f10896
nikitemailua <nikitemailua@gmail.com>
2023-04-13 12:03:24 +0300

    Corr_sem

--- addTask.php
+++ addTask.php
@@ -1,15 +1,17 @@
 <?PHP
+require("boot.php");
 $title = "веб - технологии";
 $page_title = "Вход";
 $content = "
-<form action = '' metod = 'post'>
+<form action = '' method = 'post'>
     <textarea name = 'task'></textarea>
     <input type='submit'>
 </form>
 ";
 require("content/layout.php");
-require("boot.php");
-if (isset($_SESSION['login']) and isset($_SESSION['password']) and $_POST['task']) {
+//require("boot.php");
+
+if (isset($_SESSION['login']) and isset($_SESSION['password']) and isset($_POST['task'])) {
     
     $id = $_SESSION['id'];
     $pas = $_SESSION['password'];
@@ -18,4 +20,5 @@ if (isset($_SESSION['login']) and isset($_SESSION['password']) and $_POST['task'
     // require_once __DIR__ . '/boot.php';
     $stmt = pdo()->query("INSERT INTO `tasks`(`User_id`, `Task`) VALUES ('$id','$task')");
 }
+
 ?>
