aaae6d7beeb4e1acecb0c20fb62ccad6a4a7d766
nikitemailua <nikitemailua@gmail.com>
2023-04-17 03:32:25 +0300

    Final_change

--- show_Tasks.php
+++ show_Tasks.php
@@ -13,7 +13,23 @@ while ($row = $stmt -> fetch()){
 
     //$content =  $content . $row['User_id'] . " | ". $row['Task'] . " | " ."<br>";
     $content = $content.  
-    "<form action='' method='post'> <div>".$row['User_id']. ' | '.$row['Task']."</div> <input type='checkbox' name='block' value='Yes' /></form>";
+    "<form action='' method='post'> 
+    <textarea cols = '1' readonly rows = '1' name = 'task_id'>".$row['task_id']
+    ."</textarea><textarea  name = 'Ed_task'>".$row['Task']." 
+    </textarea><input type='submit' name='change' value='Edit' />
+    <input type='submit' name='delete' value='Delete' />
+    </form>";
+}
+if(isset($_POST['task_id']) and isset($_POST['Ed_task']) and isset($_POST['change'])){
+    $task_id = $_POST['task_id'];
+    $stmt = pdo() ->query("UPDATE `tasks` SET `Task` = '".$_POST['Ed_task']."' WHERE `tasks`.`task_id` =".$task_id);
+    header("Location: ".$_SERVER['REQUEST_URI']);
+}
+
+if(isset($_POST['task_id']) and isset($_POST['delete'])){
+    $task_id = $_POST['task_id'];
+    $stmt = pdo()-> query("DELETE FROM `tasks` WHERE `tasks`.`task_id` = ".$task_id);
+    header("Location: ".$_SERVER['REQUEST_URI']);
 }
 
 }
@@ -21,4 +37,5 @@ else{
     $content = "Вы не авторизованы, либо у вас нет доступа к этой странице.";
 }
 require("content/layout.php");
+
 ?>
